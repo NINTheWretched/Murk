f7be2a208fca759045489de54f2f3ec6003db540
Jackson Pennington <jackson.w.pennington@gmail.com>
2025-11-19 13:35:03 -0500

    MURK_Alpha_0.06
    This is the 6th alpha build of MURK. This update adds collisions to NPCs.

--- murk/npc.gd
+++ murk/npc.gd
@@ -9,13 +9,13 @@ func _on_timer_timeout():
 	rng.randomize()
 	var random_int = rng.randi_range(1, 4)
 	print("Int:", random_int)
-	if random_int == 1:
+	if random_int == 1 and !$up.is_colliding():
 		_move(Vector2(0, -1))
-	elif random_int == 2:
+	elif random_int == 2 and !$down.is_colliding():
 		_move(Vector2(0, 1))
-	elif random_int == 3:
+	elif random_int == 3 and !$left.is_colliding():
 		_move(Vector2(-1, 0))
-	elif random_int == 4:
+	elif random_int == 4 and !$right.is_colliding():
 		_move(Vector2(1, 0))
 
 func _move(dir: Vector2):
